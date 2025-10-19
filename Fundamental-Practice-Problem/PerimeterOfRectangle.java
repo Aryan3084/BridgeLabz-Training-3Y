bf984fe0e648aa410dfdf4bb2c4658b8923d00f5
Aryan3084 <singhraghav3084@gmail.com>
2025-10-19 16:47:01 +0530

    [Aryan] added Fundamental-Practise-Problem

---
+++ Fundamental-Practice-Problem/PerimeterOfRectangle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double l = sc.nextDouble();
+        double w = sc.nextDouble();
+        double perimeter = 2 * (l + w);
+        System.out.println(perimeter);
+    }
+}
---
+++ Fundamental-Practice-Problem/VolumeOfCylinder.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class VolumeOfCylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        double h = sc.nextDouble();
+        double volume = Math.PI * r * r * h;
+        System.out.println(volume);
+    }
+}
